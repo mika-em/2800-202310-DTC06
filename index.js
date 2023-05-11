e70f721f35fc8c24333fcf146ea31e047612b51a
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-11 15:26:48 -0700

    Implement skeleton ejs and js for persona generator

--- index.js
+++ index.js
@@ -1,104 +1,97 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const User = require("./models/users"); // require the users.js file
-const Joi = require("joi");
+const url = require('url');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose
-  .connect(
-    "mongodb+srv://mika-em:wabisabi@teamcluster.hnn9rvs.mongodb.net/?retryWrites=true&w=majority",
-    {
-      // connect to the database
-      useNewUrlParser: true, // this is to avoid deprecation warnings
-    }
-  )
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.log("Error connecting to database: ", error);
-  });
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-); // this is to parse the body of the request
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json());
+const navLinks = [
+    { name: 'Home', link: '/' },
+    { name: 'Persona', link: '/persona' },
+];
 
-//index page
-app.get("/", (req, res) => {
-  // res.sendFile(__dirname + "/index.html");
-  res.render("index");
+const personaLinks = [
+    { name: 'General prompt presets', link: '/persona/general-prompt' },
+    { name: 'Saved prompt presets', link: '/persona/saved-prompt' },
+    { name: 'Create a new prompt preset', link: '/persona/new-prompt' },
+    { name: 'Write my own prompt', link: '/persona/chat' },
+];
+
+// placeholder for db for chatPrompt/chatHistory
+var chatPrompt = ["test"];
+var savedPromptParameter = ["hello", "world", "test"];
+
+app.use('/', (req, res, next) => {
+    app.locals.navLinks = navLinks;
+    app.locals.personaLinks = personaLinks;
+    app.locals.chatPrompt = chatPrompt;
+    app.locals.savedPromptParameter = savedPromptParameter;
+    app.locals.currentURL = url.parse(req.url).pathname;
+    next();
 });
 
-//signup page
-app.get("/signup", (req, res) => {
-  res.render("signup");
+app.get('/', (req, res) => {
+    res.render("home");
 });
 
-//signup route
-app.post("/signup", async (req, res) => {
-  const { name, username, password } = req.body;
-
-  try {
-    await User.create({
-      name: name,
-      username: username,
-      password: password,
-    });
-    console.log("User created");
-
-    res.redirect("/login");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred while creating your account.");
-  }
+app.get('/persona', (req, res) => {
+    res.render("persona");
 });
 
-//login page
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get('/persona/general-prompt', (req, res) => {
+    console.log(chatPrompt)
+    res.render("generalPrompt");
 });
 
-//login route
-app.post("/loginUser", async (req, res) => {
-  const schema = Joi.object({
-    password: Joi.string(),
-  });
+app.post('/persona/general-prompt', (req, res) => {
+    const name = req.body.name || "random";
+    const age = req.body.age || "random";
+    const gender = req.body.gender || "random";
+    const situation = req.body.plot || "random";
+    const plot = req.body.plot || "random";
 
-  try {
-    console.log(req.body.password);
-    const value = await schema.validateAsync({
-      password: req.body.password,
-    });
-  } catch (err) {
-    console.log(err);
-    console.log("The password has to be a string");
-    return;
-  }
+    const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
+    chatPrompt.push("You: " + message);
+    chatPrompt.push("hello");
+    console.log(chatPrompt)
 
-  try {
-    const result = await User.findOne({
-      username: req.body.username,
-    });
-    if (req.body.password == result.password) {
-      console.log("User authenticated");
-      res.redirect("/");
-    } else {
-      res.send("wrong password");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+    // placeholder for db for chatPrompt/chatHistory
+    res.redirect('/persona/chat');
 });
 
-app.use(express.static(__dirname + "/")); // this is to serve static files like images
+app.get('/persona/saved-prompt', (req, res) => {
+    res.render("savedPrompt");
+});
+
+app.get('/persona/new-prompt', (req, res) => {
+    res.render("newPrompt");
+});
+
+app.post('/persona/new-prompt', (req, res) => {
+    // placeholder for db for chatPrompt/chatHistory
+    const parameter = req.body.parameter;
+    savedPromptParameter.push(parameter);
+    console.log(savedPromptParameter);
+    res.render("newPrompt");
+});
+
+app.get('/persona/chat', (req, res) => {
+    // placeholder for db for chatPrompt/chatHistory
+    console.log(chatPrompt);
+    res.render("chat");
+});
+
+app.post('/persona/chat', (req, res) => {
+    // placeholder for db for chatPrompt/chatHistory
+    const message = req.body.message;
+    chatPrompt.push("You: " + message);
+    console.log(chatPrompt);
+    res.render("chat");
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });

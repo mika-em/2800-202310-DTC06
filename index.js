40c7c065322f99135d7231554ea227d1ef8c7458
mika-em <mkmanaligod@gmail.com>
2023-05-12 11:25:33 -0700

    fixed small bug in index.js

--- index.js
+++ index.js
@@ -327,8 +327,141 @@ app.post("/loginUser", async (req, res) => {
 
 app.use(express.static(__dirname + "/")); // this is to serve static files like images
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.get('/home', (req, res) => {
+  res.render("home", {
+    name : req.session.user.name,
+  });
+});
+
+app.get('/profile', (req, res) => {
+  res.render("profile");
+});
+
+app.get('/profile/account-setting', async (req, res) => {
+  const currentUser = await User.findOne({ username: "mika" });
+  const name = currentUser.name;
+  const username = currentUser.username;
+  const email = currentUser.email;
+  const securityQuestion = currentUser.securityQuestion;
+  const securityAnswer = currentUser.securityAnswer;
+  console.log(securityAnswer)
+  res.render("accountsetting", {
+    name: name,
+    username: username,
+    email: email,
+    securityQuestion: securityQuestion,
+    securityAnswer: securityAnswer,
+    disabled: true
+  });
+});
+
+app.post('/profile/account-setting', async (req, res) => {
+  const usernameInput = "mika"
+  if (req.body.action === "Edit") {
+    console.log("edit")
+    console.log(usernameInput)
+    const currentUser = await User.findOne({ username: usernameInput });
+    const name = currentUser.name;
+    const username = currentUser.username;
+    const email = currentUser.email;
+    const securityQuestion = currentUser.securityQuestion;
+    const securityAnswer = currentUser.securityAnswer;
+    res.render("accountsetting", {
+      name: name,
+      username: username,
+      email: email,
+      securityQuestion: securityQuestion,
+      securityAnswer: securityAnswer,
+      disabled: false
+    });
+  } else if (req.body.action === "Save") {
+    console.log("save")
+    const nameInput = req.body.name
+    const emailInput = req.body.email
+    const securityQuestionInput = req.body.securityQuestion
+    const securityAnswerInput = req.body.securityAnswer
+    console.log(nameInput)
+    console.log(usernameInput)
+    await User.updateOne(
+      { username: usernameInput },
+      {
+        $set: {
+          name: nameInput,
+          email: emailInput,
+          securityQuestion: securityQuestionInput,
+          securityAnswer: securityAnswerInput
+        }
+      })
+    const currentUser = await User.findOne({ username: usernameInput });
+    const name = currentUser.name;
+    const username = currentUser.username;
+    const email = currentUser.email;
+    const securityQuestion = currentUser.securityQuestion;
+    const securityAnswer = currentUser.securityAnswer;
+    res.render("accountsetting", {
+      name: name,
+      username: username,
+      email: email,
+      securityQuestion: securityQuestion,
+      securityAnswer: securityAnswer,
+      disabled: true
+    });
+  }
+});
+
+app.get('/persona', (req, res) => {
+  res.render("persona");
+});
+
+app.get('/persona/general-prompt', (req, res) => {
+  console.log(chatPrompt)
+  res.render("generalPrompt");
+});
+
+app.post('/persona/general-prompt', (req, res) => {
+  const name = req.body.name || "random";
+  const age = req.body.age || "random";
+  const gender = req.body.gender || "random";
+  const situation = req.body.plot || "random";
+  const plot = req.body.plot || "random";
+
+  const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
+  chatPrompt.push("You: " + message);
+  chatPrompt.push("hello");
+  console.log(chatPrompt)
+
+  // placeholder for db for chatPrompt/chatHistory
+  res.redirect('/persona/chat');
+});
+
+app.get('/persona/saved-prompt', (req, res) => {
+  res.render("savedPrompt");
+});
+
+app.get('/persona/new-prompt', (req, res) => {
+  res.render("newPrompt");
+});
+
+app.post('/persona/new-prompt', (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  const parameter = req.body.parameter;
+  savedPromptParameter.push(parameter);
+  console.log(savedPromptParameter);
+  res.render("newPrompt");
+});
+
+app.get('/persona/chat', (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  console.log(chatPrompt);
+  res.render("chat");
+});
+
+app.post('/persona/chat', (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  const message = req.body.message;
+  chatPrompt.push("You: " + message);
+  console.log(chatPrompt);
+  res.render("chat");
 });
 const url = require('url');
 const express = require('express');

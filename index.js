33c4baeb5975ca5c80697e63bc694fcc44466515
mika-em <mkmanaligod@gmail.com>
2023-05-12 12:31:50 -0700

    fixed logout page

--- index.js
+++ index.js
@@ -248,6 +248,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 //signup page
 app.get("/signup", (req, res) => {
   res.render("signup");
@@ -310,14 +314,14 @@ app.post("/loginUser", async (req, res) => {
   const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(loginName);
 
   const queryField = isEmail ? "email" : "username";
-  
+
   console.log(`isEmail: ${isEmail}, queryField: ${queryField}`);
-  
+
   const user = await User.findOne({
     [queryField]: loginName
   }).select('name username email password _id').exec();
   console.log(user);
-  
+
   if (!user) {
     // If user is not found, return an error message
     return res.status(400).send("Invalid email/username or password.");
@@ -331,7 +335,7 @@ app.post("/loginUser", async (req, res) => {
 
     return res.render("home", {
       name: req.session.user.name,
-    } )
+    })
   } else {
     return res.status(400).send("Invalid email/username or password.");
   }
@@ -342,7 +346,7 @@ app.use(express.static(__dirname + "/")); // this is to serve static files like 
 
 app.get('/home', (req, res) => {
   res.render("home", {
-    name : req.session.user.name,
+    name: req.session.user.name,
   });
 });
 
@@ -351,7 +355,9 @@ app.get('/profile', (req, res) => {
 });
 
 app.get('/profile/account-setting', async (req, res) => {
-  const currentUser = await User.findOne({ username: "mika" });
+  const currentUser = await User.findOne({
+    username: "mika"
+  });
   const name = currentUser.name;
   const username = currentUser.username;
   const email = currentUser.email;
@@ -373,7 +379,9 @@ app.post('/profile/account-setting', async (req, res) => {
   if (req.body.action === "Edit") {
     console.log("edit")
     console.log(usernameInput)
-    const currentUser = await User.findOne({ username: usernameInput });
+    const currentUser = await User.findOne({
+      username: usernameInput
+    });
     const name = currentUser.name;
     const username = currentUser.username;
     const email = currentUser.email;
@@ -395,17 +403,19 @@ app.post('/profile/account-setting', async (req, res) => {
     const securityAnswerInput = req.body.securityAnswer
     console.log(nameInput)
     console.log(usernameInput)
-    await User.updateOne(
-      { username: usernameInput },
-      {
-        $set: {
-          name: nameInput,
-          email: emailInput,
-          securityQuestion: securityQuestionInput,
-          securityAnswer: securityAnswerInput
-        }
-      })
-    const currentUser = await User.findOne({ username: usernameInput });
+    await User.updateOne({
+      username: usernameInput
+    }, {
+      $set: {
+        name: nameInput,
+        email: emailInput,
+        securityQuestion: securityQuestionInput,
+        securityAnswer: securityAnswerInput
+      }
+    })
+    const currentUser = await User.findOne({
+      username: usernameInput
+    });
     const name = currentUser.name;
     const username = currentUser.username;
     const email = currentUser.email;

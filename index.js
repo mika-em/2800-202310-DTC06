1639dbcd040267338a3d1b0a60e4de93e63fa158
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-12 10:16:08 -0700

    Enable account detail editing and display

--- index.js
+++ index.js
@@ -30,16 +30,195 @@ app.use("/", (req, res, next) => {
   app.locals.savedPromptParameter = savedPromptParameter;
   app.locals.currentURL = url.parse(req.url).pathname;
   next();
+app.use("/", (req, res, next) => {
+  app.locals.navLinks = navLinks;
+  app.locals.personaLinks = personaLinks;
+  app.locals.chatPrompt = chatPrompt;
+  app.locals.savedPromptParameter = savedPromptParameter;
+  app.locals.currentURL = url.parse(req.url).pathname;
+  next();
 });
 
+mongoose
+  .connect(
+    "mongodb+srv://maddy:maddymaddy@teamcluster.hnn9rvs.mongodb.net/?retryWrites=true&w=majority",
+    {
+      // connect to the database
+      useNewUrlParser: true, // this is to avoid deprecation warnings
+    }
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log("Error connecting to database: ", error);
+  });
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+); // this is to parse the body of the request
+
+app.use(express.json());
+
+//index page
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//signup page
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+//signup route
+app.post("/signup", async (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  try {
+    await User.create({
+      name: name,
+      username: username,
+      password: password,
+      email: email,
+    });
+    console.log("User created");
+
+    res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while creating your account.");
+  }
+});
+
+//login page
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//login route
+app.post("/loginUser", async (req, res) => {
+  const schema = Joi.object({
+    password: Joi.string(),
+  });
+
+  try {
+    console.log(req.body.password);
+    const value = await schema.validateAsync({
+      password: req.body.password,
+    });
+  } catch (err) {
+    console.log(err);
+    console.log("The password has to be a string");
+    return;
+  }
+
+  try {
+    const result = await User.findOne({
+      username: req.body.username,
+    });
+    if (req.body.password == result.password) {
+      console.log("User authenticated");
+      res.redirect("/");
+    } else {
+      res.send("wrong password");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.use(express.static(__dirname + "/")); // this is to serve static files like images
+
+app.get("/home", (req, res) => {
   res.render("home");
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
+app.get("/profile/account-setting", async (req, res) => {
+  const currentUser = await User.findOne({ username: "mika" });
+  const name = currentUser.name;
+  const username = currentUser.username;
+  const email = currentUser.email;
+  const securityQuestion = currentUser.securityQuestion;
+  const securityAnswer = currentUser.securityAnswer;
+  console.log(securityAnswer);
+  res.render("accountsetting", {
+    name: name,
+    username: username,
+    email: email,
+    securityQuestion: securityQuestion,
+    securityAnswer: securityAnswer,
+    disabled: true,
+  });
+});
+
+app.post("/profile/account-setting", async (req, res) => {
+  const usernameInput = "mika";
+  if (req.body.action === "Edit") {
+    console.log("edit");
+    console.log(usernameInput);
+    const currentUser = await User.findOne({ username: usernameInput });
+    const name = currentUser.name;
+    const username = currentUser.username;
+    const email = currentUser.email;
+    const securityQuestion = currentUser.securityQuestion;
+    const securityAnswer = currentUser.securityAnswer;
+    res.render("accountsetting", {
+      name: name,
+      username: username,
+      email: email,
+      securityQuestion: securityQuestion,
+      securityAnswer: securityAnswer,
+      disabled: false,
+    });
+  } else if (req.body.action === "Save") {
+    console.log("save");
+    const nameInput = req.body.name;
+    const emailInput = req.body.email;
+    const securityQuestionInput = req.body.securityQuestion;
+    const securityAnswerInput = req.body.securityAnswer;
+    console.log(nameInput);
+    console.log(usernameInput);
+    await User.updateOne(
+      { username: usernameInput },
+      {
+        $set: {
+          name: nameInput,
+          email: emailInput,
+          securityQuestion: securityQuestionInput,
+          securityAnswer: securityAnswerInput,
+        },
+      }
+    );
+    const currentUser = await User.findOne({ username: usernameInput });
+    const name = currentUser.name;
+    const username = currentUser.username;
+    const email = currentUser.email;
+    const securityQuestion = currentUser.securityQuestion;
+    const securityAnswer = currentUser.securityAnswer;
+    res.render("accountsetting", {
+      name: name,
+      username: username,
+      email: email,
+      securityQuestion: securityQuestion,
+      securityAnswer: securityAnswer,
+      disabled: true,
+    });
+  }
 });
 
 app.get("/persona", (req, res) => {
   res.render("persona");
 });
 
+app.get("/persona/general-prompt", (req, res) => {
+  console.log(chatPrompt);
+  res.render("generalPrompt");
 app.get("/persona/general-prompt", (req, res) => {
   console.log(chatPrompt);
   res.render("generalPrompt");
@@ -51,7 +230,17 @@ app.post("/persona/general-prompt", (req, res) => {
   const gender = req.body.gender || "random";
   const situation = req.body.plot || "random";
   const plot = req.body.plot || "random";
+app.post("/persona/general-prompt", (req, res) => {
+  const name = req.body.name || "random";
+  const age = req.body.age || "random";
+  const gender = req.body.gender || "random";
+  const situation = req.body.plot || "random";
+  const plot = req.body.plot || "random";
 
+  const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
+  chatPrompt.push("You: " + message);
+  chatPrompt.push("hello");
+  console.log(chatPrompt);
   const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
   chatPrompt.push("You: " + message);
   chatPrompt.push("hello");
@@ -59,8 +248,12 @@ app.post("/persona/general-prompt", (req, res) => {
 
   // placeholder for db for chatPrompt/chatHistory
   res.redirect("/persona/chat");
+  // placeholder for db for chatPrompt/chatHistory
+  res.redirect("/persona/chat");
 });
 
+app.get("/persona/saved-prompt", (req, res) => {
+  res.render("savedPrompt");
 app.get("/persona/saved-prompt", (req, res) => {
   res.render("savedPrompt");
 });
@@ -75,14 +268,30 @@ app.post("/persona/new-prompt", (req, res) => {
   savedPromptParameter.push(parameter);
   console.log(savedPromptParameter);
   res.render("newPrompt");
+app.post("/persona/new-prompt", (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  const parameter = req.body.parameter;
+  savedPromptParameter.push(parameter);
+  console.log(savedPromptParameter);
+  res.render("newPrompt");
 });
 
 app.get("/persona/chat", (req, res) => {
   // placeholder for db for chatPrompt/chatHistory
   console.log(chatPrompt);
   res.render("chat");
+app.get("/persona/chat", (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  console.log(chatPrompt);
+  res.render("chat");
 });
 
+app.post("/persona/chat", (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  const message = req.body.message;
+  chatPrompt.push("You: " + message);
+  console.log(chatPrompt);
+  res.render("chat");
 app.post("/persona/chat", (req, res) => {
   // placeholder for db for chatPrompt/chatHistory
   const message = req.body.message;
@@ -168,7 +377,8 @@ app.use("/", (req, res, next) => {
 // connect to database
 mongoose
   .connect(
-    "mongodb+srv://mika-em:wabisabi@teamcluster.hnn9rvs.mongodb.net/?retryWrites=true&w=majority", {
+    "mongodb+srv://mika-em:wabisabi@teamcluster.hnn9rvs.mongodb.net/?retryWrites=true&w=majority",
+    {
       // connect to the database
       useNewUrlParser: true, // this is to avoid deprecation warnings
     }
@@ -196,16 +406,23 @@ app.use(
     extended: false,
   })
 ); // parses bodies in urlencoded format
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+); // parses bodies in urlencoded format
 
 app.use(express.json()); // parses bodies in json format
 
 //more session stuff
-app.use(session({
-  secret: "secret",
-  resave: true,
-  saveUninitialized: false,
-  store: mongoStore,
-}), );
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: false,
+    store: mongoStore,
+  })
+);
 
 //index page
 app.get("/", (req, res) => {
@@ -221,16 +438,11 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-//signup route 
+//signup route
 app.post("/signup", async (req, res) => {
   console.log("ejs set up");
-  console.log("signup route")
-  const {
-    name,
-    username,
-    email,
-    password
-  } = req.body;
+  console.log("signup route");
+  const { name, username, email, password } = req.body;
 
   const hashedPassword = await bcrypt.hashSync(password, saltRounds);
 
@@ -257,7 +469,6 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
 //login page
 app.get("/login", (req, res) => {
   res.render("login");
@@ -265,26 +476,22 @@ app.get("/login", (req, res) => {
 
 //login route
 app.post("/loginUser", async (req, res) => {
-  const {
-    loginName,
-    password
-  } = req.body;
-  console.log(loginName, password)
+  const { loginName, password, name } = req.body;
+  console.log(loginName, password);
 
   const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(loginName);
 
   const queryField = isEmail ? "email" : "username";
-  
+
   console.log(`isEmail: ${isEmail}, queryField: ${queryField}`);
-  
+
   const user = await User.findOne({
-    [queryField]: loginName
-  }).select('name username email password _id').exec();
+    [queryField]: loginName,
+  })
+    .select("name username email password _id")
+    .exec();
   console.log(user);
 
-  if (!user) {
-    // If user is not found, return an error message
-    return res.status(400).send("Invalid email/username or password.");
   if (!user) {
     // If user is not found, return an error message
     return res.status(400).send("Invalid email/username or password.");
@@ -299,13 +506,12 @@ app.post("/loginUser", async (req, res) => {
     req.session.cookie.maxAge = expireTime;
 
     return res.render("home", {
-      name: name,
+      name: req.session.user.name,
     });
   } else {
     return res.status(400).send("Invalid email/username or password.");
   }
 });
-
 
 app.use(express.static(__dirname + "/")); // this is to serve static files like images
 

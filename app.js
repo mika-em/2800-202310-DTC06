ead3eeb6b4cfedb0e68d486eb9590c8dc39d3b92
mika-em <mkmanaligod@gmail.com>
2023-05-17 01:39:25 -0700

    added comments in app.js for clarification

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require("express");
 const app = express();
-const authRoutes = require("./routes/authorization");
-const profileRoutes = require("./routes/profile");
-const database = require("./src/database");
-const sessionConfig = require("./src/session");
-const middleware = require('./src/middleware'); 
-const persona = require("./routes/persona");
-
+const express = require("express"); 
+const database = require("./src/database"); // Database Connection
+const sessionConfig = require("./src/session"); // Session Configuration
+const middleware = require('./src/middleware'); // Middleware Configuration
+const authRoutes = require("./routes/authorization"); // includes login, signup, logout, password reset, index, and home
+const profileRoutes = require("./routes/profile"); // includes profile, account settings, and update account settings
+const persona = require("./routes/persona"); // includes persona and saved personas
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -18,8 +17,6 @@ app.use(middleware);
 app.use("/", authRoutes);
 app.use("/", profileRoutes);
 app.use("/", persona);
-
-
 
 // Database & Port Connection
 database.connect();

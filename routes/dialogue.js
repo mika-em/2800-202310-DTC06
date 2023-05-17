61467f61be9c1eb6d0e9f0518cb653e4e1ea1b9d
mika-em <mkmanaligod@gmail.com>
2023-05-17 01:37:29 -0700

    implemented routes for the dialogue feature

--- routes/dialogue.js
+++ routes/dialogue.js
@@ -9,10 +9,15 @@ router.get('/dialogue', (req, res) => {
     res.render("dialogueHome");
 });
 
-// router.get('/dialogue/new', (req, res) => {
-//     res.render("startNewDialogue");
-// });
+router.get('/dialogue/new', (req, res) => {
+    res.render("startNewDialogue");
+});
 
+router.post('/dialogue/new', (req, res) => {
+});
+
+router.get('/dialogue/saved', (req, res) => {
+    res.render("savedDialogue");
 router.post('/dialogue/new', (req, res) => {
 });
 
@@ -20,6 +25,10 @@ router.get('/dialogue/saved', (req, res) => {
     res.render("savedDialogue");
 });
 
+router.post('/dialogue/saved', (req, res) => {
+});
+
+//saved persona and new persona
 router.post('/dialogue/saved', (req, res) => {
 });
 
@@ -33,8 +42,19 @@ router.post('/dialogue/pick-a-theme', (req, res) => {
     const theme = req.body.theme;
     console.log(theme);
     res.render("pickATheme");
+router.get('/dialogue/pick-a-theme', (req, res) => {
+    res.render("pickATheme");
 });
 
+router.post('/dialogue/pick-a-theme', (req, res) => {
+    const theme = req.body.theme;
+    console.log(theme);
+    res.render("pickATheme");
+});
+
+router.get('/dialogue/pick-a-theme/inner-dialogue', (req, res) => {
+    res.render("innerDialogue");
+});
 router.get('/dialogue/pick-a-theme/inner-dialogue', (req, res) => {
     res.render("innerDialogue");
 });
@@ -44,7 +64,15 @@ router.post('dialogue/pick-a-theme/inner-dialogue', (req, res) => {
     console.log(theme);
     res.render("innerDialogue");
 });
+router.post('dialogue/pick-a-theme/inner-dialogue', (req, res) => {
+    const theme = req.body.theme;
+    console.log(theme);
+    res.render("innerDialogue");
+});
 
+router.get('/dialogue/pick-a-theme/talk-to-another-character', (req, res) => {
+    res.render("talkToAnotherCharacter");
+});
 router.get('/dialogue/pick-a-theme/talk-to-another-character', (req, res) => {
     res.render("talkToAnotherCharacter");
 });
@@ -54,7 +82,20 @@ router.post('dialogue/pick-a-theme/talk-to-another-character', (req, res) => {
     console.log(theme);
     res.render("talkToAnotherCharacter");
 });
+router.post('dialogue/pick-a-theme/talk-to-another-character', (req, res) => {
+    const theme = req.body.theme;
+    console.log(theme);
+    res.render("talkToAnotherCharacter");
+});
 
+router.get('/dialogue/pick-a-theme/chat-with-yourself', (req, res) => {
+    res.render("chatWithYourself");
+});
+
+router.post('dialogue/pick-a-theme/chat-with-yourself', (req, res) => {
+    const theme = req.body.theme;
+    console.log(theme);
+    res.render("chatWithYourself");
 router.get('/dialogue/pick-a-theme/chat-with-yourself', (req, res) => {
     res.render("chatWithYourself");
 });

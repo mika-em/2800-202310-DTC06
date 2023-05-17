9f8062559d939360359fbd3db5a28a2c24590fd1
mika-em <mkmanaligod@gmail.com>
2023-05-17 09:18:22 -0700

    implemented API in persona.js for testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express"); 
 const app = express();
 const path = require('path');
+const app = express();
 const database = require("./src/database"); // Database Connection
 const sessionConfig = require("./src/session"); // Session Configuration
 const middleware = require('./src/middleware'); // Middleware Configuration

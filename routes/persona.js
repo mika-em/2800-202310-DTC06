57698fcfaa0e6474b3d1c4edfe068e5a91403e10
mika-em <mkmanaligod@gmail.com>
2023-05-17 09:18:22 -0700

    implemented API in persona.js for testing

--- routes/persona.js
+++ routes/persona.js
@@ -1,177 +1,191 @@
 const express = require("express");
 const router = express.Router();
-const dotenv = require('dotenv');
-const User = require("../models/users");
-const { Configuration, OpenAIApi } = require('openai');
+const axios = require("axios");
 
-dotenv.config();
-
-const configuration = new Configuration({
-    organization: "org-IK9aHGvfAPS3zqJgEZurc5B7",
-    apiKey: process.env.OPENAI_API_KEY,
+router.use("/persona", async (req, res, next) => {
+  req.session.newParameter = newParameter;
+  next();
 });
 
-const openai = new OpenAIApi(configuration);
-
-async function callOpenAIAPi(userPrompt) {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${userPrompt}`,
-        temperature: 0,
-        max_tokens: 1000,
-    });
-    const responseData = response.data.choices[0].text;
-    console.log(responseData);
-    return responseData;
-}
-
-let newParameter = [];
-
-router.use('/persona', async (req, res, next) => {
-    req.session.newParameter = newParameter;
-    next();
-});
-
-router.get('/persona', (req, res) => {
-    res.render("persona");
+router.get("/persona", (req, res) => {
+  res.render("persona");
 });
 
 // General Prompt
-router.get('/persona/general-prompt', (req, res) => {
-    res.render("generalPrompt");
+router.get("/persona/general-prompt", (req, res) => {
+  res.render("generalPrompt");
 });
 
-router.post('/persona/chat/general', async (req, res) => {
-    const gender = req.body.gender || "random";
-    const name = req.body.name || "random";
-    const age = req.body.age || "random";
-    const plot = req.body.plot || "random";
+router.post("/persona/chat/general", async (req, res) => {
+  const gender = req.body.gender || "random";
+  const name = req.body.name || "random";
+  const age = req.body.age || "random";
+  const plot = req.body.plot || "random";
 
-    const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
-    chatPrompt.push("You: " + message);
-    chatPrompt.push("hello");
-    //console.log(chatPrompt)
+  const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
+  chatPrompt.push("You: " + message);
+  chatPrompt.push("hello");
+  //console.log(chatPrompt)
 
-    res.render("chat", {
-        placeholderText: "Write a prompt here...",
-        personaHistory: personaHistory
-    });
+  res.render("chat", {
+    placeholderText: "Write a prompt here...",
+    personaHistory: personaHistory,
+  });
 });
 
 // Saved Prompt Parameters
-router.get('/persona/saved-prompt', async (req, res) => {
-    const currentUser = await User.findOne({
-        username: req.session.user.username
-    });
-    const userID = currentUser._id;
+router.get("/persona/saved-prompt", async (req, res) => {
+  const currentUser = await User.findOne({
+    username: req.session.user.username,
+  });
+  const userID = currentUser._id;
 
-    const savedPromptParameter = await Parameter.find({
-        userId: userID
-    });
+  const savedPromptParameter = await Parameter.find({
+    userId: userID,
+  });
 
-    console.log(savedPromptParameter)
-    console.log(savedPromptParameter[0].parameterSet[0])
-    res.render("savedPrompt", { savedPromptParameter: savedPromptParameter });
+  console.log(savedPromptParameter);
+  console.log(savedPromptParameter[0].parameterSet[0]);
+  res.render("savedPrompt", { savedPromptParameter: savedPromptParameter });
 });
 
 // New Prompt Parameters
-router.get('/persona/new-prompt', (req, res) => {
-    res.render("newPrompt", { newParameter: newParameter });
+router.get("/persona/new-prompt", (req, res) => {
+  res.render("newPrompt", { newParameter: newParameter });
 });
 
-router.post('/persona/new-prompt', (req, res) => {
-    const parameter = req.body.parameter;
-    newParameter.push(parameter);
+router.post("/persona/new-prompt", (req, res) => {
+  const parameter = req.body.parameter;
+  newParameter.push(parameter);
+  console.log(newParameter);
+  res.render("newPrompt", { newParameter: newParameter });
+});
+
+router.post("/persona/new-prompt/delete", (req, res) => {
+  const { index } = req.body;
+  if (index >= 0 && index < newParameter.length) {
+    newParameter.splice(index, 1);
     console.log(newParameter);
     res.render("newPrompt", { newParameter: newParameter });
+  } else {
+    res.status(400).send("Invalid index"); // Send an error response to the client
+  }
 });
 
-router.post('/persona/new-prompt/delete', (req, res) => {
-    const { index } = req.body;
-    if (index >= 0 && index < newParameter.length) {
-        newParameter.splice(index, 1);
-        console.log(newParameter);
-        res.render("newPrompt", { newParameter: newParameter });
-    } else {
-        res.status(400).send('Invalid index'); // Send an error response to the client
-    }
+router.post("/persona/new-prompt/saved", async (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const parameterSet = newParameter;
+  const date = new Date();
+
+  const currentUser = await User.findOne({
+    username: req.session.user.username,
+  });
+
+  await Parameter.create({
+    userId: currentUser._id,
+    title: title,
+    description: description,
+    parameterSet: parameterSet,
+    date: date,
+  });
+  res.render("newPrompt", { newParameter: newParameter });
 });
 
+router.get("/persona/chat", async (req, res) => {
+  const currentUser = await User.findOne({
+    username: req.session.user.username,
+  });
+  const personaHistory = currentUser.personaHistory;
 
-
-router.post('/persona/new-prompt/saved', async (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    const parameterSet = newParameter;
-    const date = new Date();
-
-    const currentUser = await User.findOne({
-        username: req.session.user.username
-    });
-
-    await Parameter.create({
-        userId: currentUser._id,
-        title: title,
-        description: description,
-        parameterSet: parameterSet,
-        date: date
-    });
-    res.render("newPrompt", { newParameter: newParameter })
-});
-
-router.get('/persona/chat', async (req, res) => {
-    const currentUser = await User.findOne({
-        username: req.session.user.username
-    });
-    const personaHistory = currentUser.personaHistory
-
-    res.render("chat", {
-        placeholderText: "Write a prompt here...",
-        personaHistory: personaHistory
-    });
+  res.render("chat", {
+    placeholderText: "Write a prompt here...",
+    personaHistory: personaHistory,
+  });
 });
 
 async function callOpenAIAPi(userPrompt) {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${userPrompt}`,
-        temperature: 0,
-        max_tokens: 1000,
-    });
-    const responseData = response.data.choices[0].text;
-    console.log(responseData);
-    return responseData;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${userPrompt}`,
+    temperature: 0,
+    max_tokens: 1000,
+  });
+  const responseData = response.data.choices[0].text;
+  console.log(responseData);
+  return responseData;
 }
 
-router.post('/persona/chat', async (req, res) => {
-    const prompt = req.body.prompt;
-    const currentUsername = req.session.user.username;
-    console.log(prompt);
+router.post("/persona/chat", async (req, res) => {
+  // placeholder for db for chatPrompt/chatHistory
+  // const message = req.body.message;
+  // chatPrompt.push("You: " + message);
+  // console.log(chatPrompt);
+  // res.render("chat");
 
-    const responseData = await callOpenAIAPi(prompt);
+  // try {
+  //     const {
+  //         prompt
+  //     } = req.body;
 
-    await User.updateOne({
-        username: currentUsername
-    }, {
-        $push: {
-            personaHistory: {
-                userPrompt: prompt,
-                botResponse: responseData
-            }
-        }
-    })
-    const currentUser = await User.findOne({
-        username: req.session.user.username
-    });
-    const personaHistory = currentUser.personaHistory
+  //     // Make a request to the Chat Completions API endpoint
+  //     const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+  //         model: 'gpt-3.5-turbo',
+  //         messages: [{
+  //             role: 'system',
+  //             content: 'You are a persona generator.'
+  //         }, {
+  //             role: 'user',
+  //             content: prompt
+  //         }],
+  //     }, {
+  //         headers: {
+  //             'Authorization': 'Bearer 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',
+  //             'Content-Type': 'application/json',
+  //         },
+  //     });
 
-    console.log(personaHistory)
+  const { Configuration, OpenAIApi } = require("openai");
 
-    res.render("chat", { 
-        placeholderText: "Write a prompt here...",
-        personaHistory: personaHistory
-     });
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a persona generator.",
+      },
+      {
+        role: "user",
+        content: req.body,
+      },
+    ],
+  });
+  console.log(completion.data.choices[0].message);
 });
 
+// Process the response and extract the generated message
+//         const {
+//             choices
+//         } = response.data;
+//         const generatedMessage = choices[0].message.content;
+
+//         res.json({
+//             message: generatedMessage
+//         });
+//     } catch (error) {
+//         console.error('Error generating persona:', error);
+//         res.status(500).json({
+//             error: 'Failed to generate persona'
+//         });
+//     }
+// });
+
+var chatPrompt = ["test"];
+var savedPromptParameter = ["hello", "world", "test"];
 
 module.exports = router;

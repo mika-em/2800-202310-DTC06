8de8381f63bf71444b5145c80b1332a8a4ba0276
Fiona Wong <fwwong01@gmail.com>
2023-05-17 12:03:17 -0700

    Implement 401  and 404 error pages

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const middleware = require('./src/middleware'); // Middleware Configuration
 const authRoutes = require("./routes/authorization"); // includes login, signup, logout, password reset, index, and home
 const profileRoutes = require("./routes/profile"); // includes profile, account settings, and update account settings
 const persona = require("./routes/persona"); // includes persona and saved personas
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(sessionConfig);
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use(middleware);
@@ -25,6 +27,18 @@ app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
 
+// Serve CSS files from the "public/css" directory
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+// Serve JS files from the "public/js" directory
 app.use(express.static(__dirname + "/"));
+
+//404 page not found 
+app.get("*", (req, res) => {
+    res.render("../views/error/404");
+});
+
+app.post("/404", (req, res) => {
+    res.status(404).redirect("/");
+});
 
 module.exports = app;

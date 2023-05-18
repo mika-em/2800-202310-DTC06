6663dfb86fee64716f5f52e72001fd80553c1115
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-17 21:01:54 -0700

    Update skeleton frontend and backend to function like a chat

---
+++ models/chat.js
@@ -0,0 +1,118 @@
+// import bot from './assets/bot.svg'
+// import user from './assets/user.svg'
+
+// const { clear } = require("console");
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chatContainer');
+
+let loadInterval;
+
+function loader(element) {
+    element.textContent = '';
+
+    loadInterval = setInterval(() => {
+        element.textContent += '.';
+
+        if (element.textContent.length === 4) {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++;
+        } else {
+            clearInterval(interval)
+        }
+    }, 20);
+}
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+// in between className="profile"
+// <img
+//     src="${isAi ? bot : user}"
+//     alt="${isAi ? 'bot' : 'user'}"
+// />
+
+function chatStripe(isAi, value, uniqueId) {
+    return `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+}
+
+const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    console.log("test")
+
+    const data = new FormData(form);
+
+    // user chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    form.reset();
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId);
+
+    loader(messageDiv);
+
+    // fetch data from server -> bot's response
+    const response = await fetch('/persona/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    });
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        typeText(messageDiv, parsedData);
+    } else {
+        const error = await response.text();
+
+        messageDiv.innerHTML = "Something went wrong";
+        
+        alert(error)
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
+// form.addEventListener('keyup', (event) => {
+//     if (event.keyCode === 13) {
+//         handleSubmit(event);
+//     }
+// });

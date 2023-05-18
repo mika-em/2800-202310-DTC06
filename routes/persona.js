ee8cc17e5fbeab4d91b0623a4fba7ef8d74ec540
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-17 21:04:11 -0700

    Implement OpenAI api key and corresponding calls

--- routes/persona.js
+++ routes/persona.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
+const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require('openai');
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
 
+dotenv.config();
+
+const configuration = new Configuration({
+    organization: "org-IK9aHGvfAPS3zqJgEZurc5B7",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+router.use(express.json());
+router.use(cors());
+
+const promptCache = {};
 
 router.get('/persona', (req, res) => {
     res.render("./persona/persona");
@@ -25,7 +42,7 @@ router.post('/persona/general-prompt', (req, res) => {
     console.log(chatPrompt)
 
     // placeholder for db for chatPrompt/chatHistory
-    res.redirect('/persona/chat');
+    res.redirect('/persona/chat', { placeholderText: "Write a prompt here..." });
 });
 
 router.get('/persona/saved-prompt', (req, res) => {
@@ -46,84 +63,82 @@ router.post('/persona/new-prompt', (req, res) => {
 
 router.get('/persona/chat', (req, res) => {
     // placeholder for db for chatPrompt/chatHistory
-    console.log(chatPrompt);
-    res.render("chat");
+    // console.log(chatPrompt);
+    res.render("chat", { placeholderText: "Write a prompt here..." });
 });
 
-router.post('/persona/chat', async (req, res) => {
-    // placeholder for db for chatPrompt/chatHistory
-    // const message = req.body.message;
-    // chatPrompt.push("You: " + message);
-    // console.log(chatPrompt);
-    // res.render("chat");
+// Rate limiting configuration
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 3500, // 5 requests per minute
+});
+
+// debounce mechanism to prevent too many API calls
+let typingTimeout;
+let tokensUsed = 0;
+const maxTokensPerRequest = 10;
+
+router.post('/persona/chat', limiter, async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        clearTimeout(typingTimeout); // clear the timeout on every request
+
+        // check if the response is available in the cache
+        if (prompt in promptCache) {
+            console.log("Response from cache")
+            res.status(200).send({
+                bot: promptCache[prompt],
+            });
+        } else {
+            // Check if the remaining tokens can accommodate the request
+            const remainingTokens = 90000 - tokensUsed;
+            const tokensNeeded = prompt.length + maxTokensPerRequest;
+            if (tokensNeeded > remainingTokens) {
+                console.error('Token limit exceeded');
+                res.status(429).send({ error: 'Token limit exceeded. Please try again later.' });
+                return;
+            }
+
+            typingTimeout = setTimeout(async () => {
+                // wait for 500ms of inactivity before making the API call
+                try {
+
+                    const response = await openai.createCompletion({
+                        model: "gpt-3.5-turbo",
+                        prompt: `${prompt}`,
+                        temperature: 0.5,
+                        // max_tokens: 64,
+                        // top_p: 1,
+                        // frequency_penalty: 0.5,
+                        // presence_penalty: 0.5,
+                    });
+
+                    const botResponse = response.data.choices[0].text;
+
+                    // store the response in the cache
+                    promptCache[prompt] = botResponse;
+
+                    console.log("Response from API")
+                    res.status(200).send({
+                        bot: botResponse,
+                    });
+                    // Update the tokens used count
+                    tokensUsed += prompt.length + botResponse.length;
+                } catch (error) {
+                    console.log("Error from OpenAI API", error);
+                    res.status(500).send({ error: 'Failed to generate bot response' });
+                }
+            }, 500);
+        }
+    } catch (error) {
+        console.log("Internal Server Error", error)
+        res.status(500).send({ error })
+    }
+});
 
 
-    // try {
-    //     const {
-    //         prompt
-    //     } = req.body;
-
-    //     // Make a request to the Chat Completions API endpoint
-    //     const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-    //         model: 'gpt-3.5-turbo',
-    //         messages: [{
-    //             role: 'system',
-    //             content: 'You are a persona generator.'
-    //         }, {
-    //             role: 'user',
-    //             content: prompt
-    //         }],
-    //     }, {
-    //         headers: {
-    //             'Authorization': 'Bearer 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',
-    //             'Content-Type': 'application/json',
-    //         },
-    //     });
-
-        const {
-            Configuration,
-            OpenAIApi
-        } = require("openai");
-
-        const configuration = new Configuration({
-            apiKey: "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",
-        });
-        const openai = new OpenAIApi(configuration);
-
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{
-                role: 'system',
-                content: 'You are a persona generator.'
-            }, {
-                role: 'user',
-                content: req.body
-            }],
-        });
-        console.log(completion.data.choices[0].message);
-    });
-
-
-
-        // Process the response and extract the generated message
-//         const {
-//             choices
-//         } = response.data;
-//         const generatedMessage = choices[0].message.content;
-
-//         res.json({
-//             message: generatedMessage
-//         });
-//     } catch (error) {
-//         console.error('Error generating persona:', error);
-//         res.status(500).json({
-//             error: 'Failed to generate persona'
-//         });
-//     }
-// });
-
-
-var chatPrompt = ["test"];
-var savedPromptParameter = ["hello", "world", "test"];
+// var chatPrompt = ["test"];
+// var savedPromptParameter = ["hello", "world", "test"];
 
 module.exports = router;

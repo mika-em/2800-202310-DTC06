baef5cd62723d377d5b377e00828024963b04541
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-18 00:48:31 -0700

    Modify code to simplify persona generator api call and output

--- models/users.js
+++ models/users.js
@@ -1,34 +1,31 @@
 const mongoose = require("mongoose");
-
-//defines the user schema
 const userSchema = new mongoose.Schema(
-    {
-        name: String,
-        username: String,
-        password: String,
-        email: String,
-        securityQuestion: String,
-        securityAnswer: String,
+  {
+    name: String,
+    username: String,
+    password: String,
+    email: String,
+    securityQuestion: String,
+    securityAnswer: String,
+    personaHistory: Array,
     profilePicture: {
-        data: Buffer,
-        contentType: String,
+      data: Buffer,
+      contentType: String,
     },
-        personaHistory: Array,
-        dialogueHistory: Array,
-        dialogueHistory: Array,
-        filter: {
-            default: Boolean,
-            status: Boolean,
-            class: Boolean,
-            drop: Boolean,
-            race: Boolean,
-        },
+    personaHistory: Array,
+    dialogueHistory: Array,
+    dialogueHistory: Array,
+    filter: {
+      default: Boolean,
+      status: Boolean,
+      class: Boolean,
+      drop: Boolean,
+      race: Boolean,
     },
-    // this is the name of the collection in the database
-    { collection: 'users' }
+  },
+  // this is the name of the collection in the database
+  { collection: "users" }
 );
-
-
 
 // defines the dialouge schema
 const dialogueSchema = new mongoose.Schema(
@@ -36,17 +33,23 @@ const dialogueSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     dialogueSaved: Array,
   },
-  { collection: "dialogue" }
+  { collection: "users" }
 ); //this is the name of the collection in the database
 
 const usersModel = mongoose.model("User", userSchema);
+const usersModel = mongoose.model("User", userSchema);
 
+const dialogueModel = mongoose.model("Dialogue", dialogueSchema);
 const dialogueModel = mongoose.model("Dialogue", dialogueSchema);
 
 module.exports = {
+  usersModel,
+  dialogueModel,
   usersModel,
   dialogueModel,
 };
--- routes/persona.js
+++ routes/persona.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
+const User = require("../models/users");
 const { Configuration, OpenAIApi } = require("openai");
-const rateLimit = require("express-rate-limit");
-const cors = require("cors");
 
 dotenv.config();
 
@@ -13,24 +12,14 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
-
-router.use(express.json());
-router.use(cors());
-const axios = require("axios");
-
-router.use("/persona", async (req, res, next) => {
-  req.session.newParameter = newParameter;
-  next();
-});
 
 router.get("/persona", (req, res) => {
-  res.render("persona");
+  res.render("./persona/persona");
 });
 
 // General Prompt
 router.get("/persona/general-prompt", (req, res) => {
-  res.render("generalPrompt");
+  res.render("./persona/generalPrompt");
 });
 
 router.post("/persona/chat/general", async (req, res) => {
@@ -38,11 +27,44 @@ router.post("/persona/chat/general", async (req, res) => {
   const name = req.body.name || "random";
   const age = req.body.age || "random";
   const plot = req.body.plot || "random";
+router.post("/persona/chat/general", async (req, res) => {
+  const gender = req.body.gender || "random";
+  const name = req.body.name || "random";
+  const age = req.body.age || "random";
+  const plot = req.body.plot || "random";
 
-  const message = `Generate a ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting where they are faced with ${situation}.`;
-  chatPrompt.push("You: " + message);
-  chatPrompt.push("hello");
-  //console.log(chatPrompt)
+  console.log("gender", gender);
+  console.log("name", name);
+  console.log("age", age);
+  console.log("plot", plot);
+  const prompt = `Generate a random ${gender} character whose name is ${name} and age is ${age}, and is in a ${plot} setting.`;
+  console.log(prompt);
+
+  const responseData = await callOpenAIAPi(prompt);
+
+  const currentUsername = req.session.user.username;
+  console.log(currentUsername);
+
+  await User.updateOne(
+    {
+      username: currentUsername,
+    },
+    {
+      $push: {
+        personaHistory: {
+          userPrompt: prompt,
+          botResponse: responseData,
+        },
+      },
+    }
+  );
+
+  const currentUser = await User.findOne({
+    username: currentUsername,
+  });
+
+  const personaHistory = currentUser.personaHistory;
+  console.log(personaHistory);
 
   res.render("chat", {
     placeholderText: "Write a prompt here...",
@@ -56,6 +78,11 @@ router.get("/persona/saved-prompt", async (req, res) => {
     username: req.session.user.username,
   });
   const userID = currentUser._id;
+router.get("/persona/saved-prompt", async (req, res) => {
+  const currentUser = await User.findOne({
+    username: req.session.user.username,
+  });
+  const userID = currentUser._id;
 
   const savedPromptParameter = await Parameter.find({
     userId: userID,
@@ -63,19 +90,21 @@ router.get("/persona/saved-prompt", async (req, res) => {
 
   console.log(savedPromptParameter);
   console.log(savedPromptParameter[0].parameterSet[0]);
-  res.render("savedPrompt", { savedPromptParameter: savedPromptParameter });
+  res.render("./persona/savedPrompt", {
+    savedPromptParameter: savedPromptParameter,
+  });
 });
 
 // New Prompt Parameters
 router.get("/persona/new-prompt", (req, res) => {
-  res.render("newPrompt", { newParameter: newParameter });
+  res.render("./persona/newPrompt", { newParameter: newParameter });
 });
 
 router.post("/persona/new-prompt", (req, res) => {
   const parameter = req.body.parameter;
   newParameter.push(parameter);
   console.log(newParameter);
-  res.render("newPrompt", { newParameter: newParameter });
+  res.render("./persona/newPrompt", { newParameter: newParameter });
 });
 
 router.post("/persona/new-prompt/delete", (req, res) => {
@@ -83,7 +112,7 @@ router.post("/persona/new-prompt/delete", (req, res) => {
   if (index >= 0 && index < newParameter.length) {
     newParameter.splice(index, 1);
     console.log(newParameter);
-    res.render("newPrompt", { newParameter: newParameter });
+    res.render("./persona/newPrompt", { newParameter: newParameter });
   } else {
     res.status(400).send("Invalid index"); // Send an error response to the client
   }
@@ -106,7 +135,7 @@ router.post("/persona/new-prompt/saved", async (req, res) => {
     parameterSet: parameterSet,
     date: date,
   });
-  res.render("newPrompt", { newParameter: newParameter });
+  res.render("./persona/newPrompt", { newParameter: newParameter });
 });
 
 router.get("/persona/chat", async (req, res) => {
@@ -121,78 +150,49 @@ router.get("/persona/chat", async (req, res) => {
   });
 });
 
-// Rate limiting configuration
-const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 3500, // 5 requests per minute
-});
+async function callOpenAIAPi(userPrompt) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${userPrompt}`,
+    temperature: 0,
+    max_tokens: 1000,
+  });
+  const responseData = response.data.choices[0].text;
+  console.log(responseData);
+  return responseData;
+}
 
-// debounce mechanism to prevent too many API calls
-let typingTimeout;
-let tokensUsed = 0;
-const maxTokensPerRequest = 10;
+router.post("/persona/chat", async (req, res) => {
+  const prompt = req.body.prompt;
+  const currentUsername = req.session.user.username;
+  console.log(prompt);
 
-router.post("/persona/chat", limiter, async (req, res) => {
-  try {
-    const prompt = req.body.prompt;
+  const responseData = await callOpenAIAPi(prompt);
 
-    clearTimeout(typingTimeout); // clear the timeout on every request
-
-    // check if the response is available in the cache
-    if (prompt in promptCache) {
-      console.log("Response from cache");
-      res.status(200).send({
-        bot: promptCache[prompt],
-      });
-    } else {
-      // Check if the remaining tokens can accommodate the request
-      const remainingTokens = 90000 - tokensUsed;
-      const tokensNeeded = prompt.length + maxTokensPerRequest;
-      if (tokensNeeded > remainingTokens) {
-        console.error("Token limit exceeded");
-        res
-          .status(429)
-          .send({ error: "Token limit exceeded. Please try again later." });
-        return;
-      }
-
-      typingTimeout = setTimeout(async () => {
-        // wait for 500ms of inactivity before making the API call
-        try {
-          const response = await openai.createCompletion({
-            model: "gpt-3.5-turbo",
-            prompt: `${prompt}`,
-            temperature: 0.5,
-            // max_tokens: 64,
-            // top_p: 1,
-            // frequency_penalty: 0.5,
-            // presence_penalty: 0.5,
-          });
-
-          const botResponse = response.data.choices[0].text;
-
-          // store the response in the cache
-          promptCache[prompt] = botResponse;
-
-          console.log("Response from API");
-          res.status(200).send({
-            bot: botResponse,
-          });
-          // Update the tokens used count
-          tokensUsed += prompt.length + botResponse.length;
-        } catch (error) {
-          console.log("Error from OpenAI API", error);
-          res.status(500).send({ error: "Failed to generate bot response" });
-        }
-      }, 500);
+  await User.updateOne(
+    {
+      username: currentUsername,
+    },
+    {
+      $push: {
+        personaHistory: {
+          userPrompt: prompt,
+          botResponse: responseData,
+        },
+      },
     }
-  } catch (error) {
-    console.log("Internal Server Error", error);
-    res.status(500).send({ error });
-  }
-});
+  );
+  const currentUser = await User.findOne({
+    username: req.session.user.username,
+  });
+  const personaHistory = currentUser.personaHistory;
 
-// var chatPrompt = ["test"];
-// var savedPromptParameter = ["hello", "world", "test"];
+  console.log(personaHistory);
+
+  res.render("chat", {
+    placeholderText: "Write a prompt here...",
+    personaHistory: personaHistory,
+  });
+});
 
 module.exports = router;

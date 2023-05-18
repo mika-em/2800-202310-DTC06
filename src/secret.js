5802fd82a239fdd6776d1252f45a9d951cbce8f1
Fiona Wong <fwwong01@gmail.com>
2023-05-18 11:45:26 -0700

    Added another easter egg location

--- src/secret.js
+++ src/secret.js
@@ -4,8 +4,8 @@ function secret() {
     //add the secret class 
     $('#login').addClass('secret');
     $('#login').removeAttr('id');
-    $('#meow').append('<audio autoplay loop><source src="../images/meow.mp3" type="audio/mpeg" loop="true"></audio>')
+    $('#meow').append('<audio autoplay loop><source src="../images/meow.mp3" type="audio/mpeg" loop="true"></audio>');
     $('.bg').append('<video autoplay muted loop id="meow"><source src="./images/meow.mp4" type="video/mp4" /></video>');
-    // $('body').addClass('meowpointer');
-    $('body').style.cursor = "url('../images/nyancat.png'), auto"
+    $('body').addClass('flash');
 }
+

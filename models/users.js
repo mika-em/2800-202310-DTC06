23ad7ba0de8e2e98448bdc11321f44dd4bd4ff8f
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-19 09:11:09 -0700

    Reformat users.js for readability

--- models/users.js
+++ models/users.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //defines the user schema
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: String,
     username: String,
     password: String,
@@ -11,36 +12,37 @@ const userSchema = new mongoose.Schema({
     personaHistory: Array,
     dialogueHistory: Array,
     profilePicture: {
-        fileName: String,
-        contentType: String,
+      fileName: String,
+      contentType: String,
     },
     filter: {
-        default: Boolean,
-        status: Boolean,
-        class: Boolean,
-        drop: Boolean,
-        race: Boolean,
-    }
-},
-    { collection: 'users' }); //this is the name of the collection in the database
+      default: Boolean,
+      status: Boolean,
+      class: Boolean,
+      drop: Boolean,
+      race: Boolean,
+    },
+  },
+  { collection: "users" }
+); //this is the name of the collection in the database
 
 // defines the dialouge schema
-const dialogueSchema = new mongoose.Schema({
+const dialogueSchema = new mongoose.Schema(
+  {
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     dialogueSaved: Array,
-    },
-    { collection: 'dialogue' }); //this is the name of the collection in the database
+  },
+  { collection: "dialogue" }
+); //this is the name of the collection in the database
 
+const usersModel = mongoose.model("User", userSchema);
 
-const usersModel = mongoose.model('User', userSchema);
-
-const dialogueModel = mongoose.model('Dialogue', dialogueSchema);
+const dialogueModel = mongoose.model("Dialogue", dialogueSchema);
 
 module.exports = {
-    usersModel,
-    dialogueModel
+  usersModel,
+  dialogueModel,
 };
-

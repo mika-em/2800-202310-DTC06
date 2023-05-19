15a6f742cac591832fe34b7a4b11ffb0d52d0c33
Fiona Wong <fwwong01@gmail.com>
2023-05-18 17:04:42 -0700

    minor fixes on about us and faq

--- routes/saved.js
+++ routes/saved.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/saved', (req, res) => {
-    res.render("../views/saved/saved");
+router.get("/saved", (req, res) => {
+  res.render("../views/saved/saved");
 });
 
-router.get('/filters', (req, res) => {
-    res.render("../views/saved/filters");
+router.get("/filters", (req, res) => {
+  res.render("../views/saved/filters");
 });
 
-router.get('/saved/dialogue', (req, res) => {
-    res.render("../views/saved/saved-dialogue");
+router.get("/saved/dialogue", (req, res) => {
+  res.render("../views/saved/saved-dialogue");
 });
 
-router.get('/saved/persona', (req, res) => {
-    res.render("../views/saved/saved-persona");
+router.get("/saved/persona", (req, res) => {
+  res.render("../views/saved/saved-persona");
 });
-
 
 module.exports = router;

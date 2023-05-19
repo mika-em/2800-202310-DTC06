eef89c09fd63619c13a2ce5409b583154d916fe5
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-19 13:29:06 -0700

    Add a mongoose schema for saved persona collection

---
+++ models/personaList.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// parameter collection schema
+const personaSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        date: Date,
+        persona: String,
+    },
+    // name of the collection in database
+    { collection: 'personas' }
+);
+
+
+const personaModel = mongoose.model('Persona', personaSchema)
+
+module.exports = personaModel;

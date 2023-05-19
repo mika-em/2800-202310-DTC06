f4a2fe5b1d386bb507caec1b40d737d46793418b
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-19 09:11:09 -0700

    Reformat users.js for readability

--- models/users.js
+++ models/users.js
@@ -1,23 +1,31 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    name: String,
-    username: String,
-    password: String,
-    email: String,
-    securityQuestion: String,
-    securityAnswer: String,
-    personaHistory: Array,
-    filter: {
-        default: Boolean,
-        status: Boolean,
-        class: Boolean,
-        drop: Boolean,
-        race: Boolean,
+
+// users collection schema
+const userSchema = new mongoose.Schema(
+    {
+        name: String,
+        username: String,
+        password: String,
+        email: String,
+        securityQuestion: String,
+        securityAnswer: String,
+        personaHistory: Array,
+        promptParameters: Array,
+        filter: {
+            default: Boolean,
+            status: Boolean,
+            class: Boolean,
+            drop: Boolean,
+            race: Boolean,
+        },
+        securityQuestion: String,
+        securityAnswer: String,
     },
-    securityQuestion: String,
-    securityAnswer: String,
-},
-    { collection: 'users' }); //this is the name of the collection in the database
+    //this is the name of the collection in the database
+    { collection: 'users' }
+);
+
+
 
 
 const usersModel = mongoose.model('User', userSchema);

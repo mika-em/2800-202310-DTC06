f4331720fe394411b5f0d8f10c7fc361c78a27b8
mika-em <mkmanaligod@gmail.com>
2023-05-19 01:20:02 -0700

    added persona input in dialogue function

--- routes/dialogue.js
+++ routes/dialogue.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const openai = require('openai');
+// const openai = require('openai');
 const User = require("../models/users").usersModel;
+const dotenv = require('dotenv');
+// const User = require("../models/users");
+const { Configuration, OpenAIApi } = require('openai');
+
+dotenv.config();
 
 
+const configuration = new Configuration({
+    organization: "org-wZOT14YD6omEzAgdgaFU5gz3",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-openai.apiKey = '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'
 
 router.get('/dialogue', (req, res) => {
     res.render("dialogueHome");
 });
 
-// router.get('/dialogue/new', (req, res) => {
-//     res.render("startNewDialogue");
-// });
+router.get('/dialogue/new', (req, res) => {
+    res.render("dialogueFilters");
+});
 
 router.get('/dialogueFilters', (req, res) => {
     res.render("dialogueFilters", {
@@ -22,10 +31,8 @@ router.get('/dialogueFilters', (req, res) => {
 });
 
 router.get('/dialogue/inner-dialogue', async (req, res) => {
-    const currentUser = await User.findOne({
-        username: req.session.user.username
-    });
-    const dialogueHistory = currentUser.dialogueHistory
+    const currentUser = await User.findOne({ username: req.session.user.username });
+    const dialogueHistory = currentUser.dialogueHistory;
 
     res.render("dialogueChat", {
         placeholderText: "Write a prompt here...",
@@ -33,10 +40,10 @@ router.get('/dialogue/inner-dialogue', async (req, res) => {
     });
 });
 
-async function callOpenAIAPi(userPrompt) {
+async function callOpenAIAPi(userPrompt, persona) {
     const response = await openai.createCompletion({
-        model: "gpt-3.5-turbo",
-        prompt: `${userPrompt}`,
+        model: "text-davinci-003",
+        prompt: `${persona}\n${userPrompt}`,
         temperature: 0,
         max_tokens: 1000,
     });
@@ -48,28 +55,32 @@ async function callOpenAIAPi(userPrompt) {
 router.post('/dialogue/inner-dialogue', async (req, res) => {
     const prompt = req.body.prompt;
     const currentUsername = req.session.user.username;
+
+    const currentUser = await User.findOne({ username: req.session.user.username });
+    const persona = currentUser.persona;
+
     console.log(prompt);
 
-    const responseData = await callOpenAIAPi(prompt);
+    const responseData = await callOpenAIAPi(prompt, persona);
 
-    await User.updateOne({
-        username: currentUsername
-    }, {
-        $push: {
-            dialogueHistory: {
-                userPrompt: prompt,
-                botResponse: responseData
+    await User.updateOne(
+        { username: currentUsername },
+        {
+            $push: {
+                dialogueHistory: {
+                    userPrompt: prompt,
+                    botResponse: responseData,
+                    persona: persona
+                }
             }
         }
-    })
-    const currentUser = await User.findOne({
-        username: req.session.user.username
-    });
-    const dialogueHistory = currentUser.dialogueHistory
+    );
 
-    console.log(personaHistory)
+    const dialogueHistory = currentUser.dialogueHistory;
 
-    res.render("chat", {
+    console.log(dialogueHistory);
+
+    res.render("dialogueChat", {
         placeholderText: "Write a prompt here...",
         dialogueHistory: dialogueHistory
     });

4b51d40bbc1180d55bdfd9977d61214cfd6f2d53
Fiona Wong <fwwong01@gmail.com>
2023-05-18 17:14:07 -0700

    Made skeleton pages

--- routes/saved.js
+++ routes/saved.js
@@ -5,6 +5,17 @@ router.get('/saved', (req, res) => {
     res.render("../views/saved/saved");
 });
 
+router.get('/filters', (req, res) => {
+    res.render("../views/saved/filters");
+});
+
+router.get('/saved/dialogue', (req, res) => {
+    res.render("../views/saved/saved-dialogue");
+});
+
+router.get('/saved/persona', (req, res) => {
+    res.render("../views/saved/saved-persona");
+});
 
 
 module.exports = router;

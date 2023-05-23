ffb0361a3584810c5b3310f5b1301ffef8804689
Fiona Wong <fwwong01@gmail.com>
2023-05-23 15:38:38 -0700

    Applied dark mode nav bar

--- src/darkmode.js
+++ src/darkmode.js
@@ -5,13 +5,27 @@
 
 function darkMode() {
     console.log("dark mode is on");
+    $(".navbar").removeClass("bg-light").addClass("bg-dark");
+    $("#home").removeAttribute("src");
+    $("#home").setAttribute("src", "../images/invsona/home-light.png");
     
-
-
+  
 }  
+
+function lightMode(){
+    console.log("dark mode is off");
+    $(".navbar").removeClass("bg-dark").addClass("bg-light");
+    $("#home").removeAttribute("src")
+    $("#home").setAttribute("src", "../images/invsona/home.png");
+}
 
 $("#dark").on("change", function() {
     if ($(this).is(":checked")) {
+      $("#dark").attr("checked", true);
       darkMode();
+    }
+    else {
+      $("#dark").attr("checked", false);
+      lightMode();
     }
   });

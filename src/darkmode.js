daaf1ea7e54e7f0b11051f8513a81b2f9299ff0f
Fiona Wong <fwwong01@gmail.com>
2023-05-23 16:12:05 -0700

    Implied dark mode saving in local storage

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,31 +1,41 @@
-//when dark mode is turn on:
-// 1. nav bar will change: symbol and text
-// 2. background will change: internal, persona, dialogue
 
-
-function darkMode() {
-    console.log("dark mode is on");
-    $(".navbar").removeClass("bg-light").addClass("bg-dark");
-    $("#home").removeAttribute("src");
-    $("#home").setAttribute("src", "../images/invsona/home-light.png");
-    
-  
-}  
-
-function lightMode(){
-    console.log("dark mode is off");
-    $(".navbar").removeClass("bg-dark").addClass("bg-light");
-    $("#home").removeAttribute("src")
-    $("#home").setAttribute("src", "../images/invsona/home.png");
+// Function to enable dark mode
+function enableDarkMode() {
+  $("#home").attr("src", "../images/invsona/invsona.png");
+  $(".backnav").removeClass("text-bg-light").addClass("text-bg-dark");
+  $(".navbar").removeClass("bg-light").addClass("bg-dark");
 }
 
+// Function to disable dark mode
+function disableDarkMode() {
+  $("#home").attr("src", "../images/invsona/home.png");
+  $(".backnav").removeClass("text-bg-dark").addClass("text-bg-light");
+  $(".navbar").removeClass("bg-dark").addClass("bg-light");
+}
+
+// Check if dark mode is enabled in local storage
+const isDarkModeEnabled = localStorage.getItem("darkMode") === "on";
+
+// Set the initial state of the dark mode switch based on local storage
+$("#dark").prop("checked", isDarkModeEnabled);
+
+// Apply dark mode or light mode based on the initial state
+if (isDarkModeEnabled) {
+  $("#dark").addClass("active");
+  enableDarkMode();
+} else {
+  disableDarkMode();
+}
+
+// Event handler for dark mode switch
 $("#dark").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("#dark").attr("checked", true);
-      darkMode();
-    }
-    else {
-      $("#dark").attr("checked", false);
-      lightMode();
-    }
-  });
+  if ($(this).is(":checked")) {
+    $(this).addClass("active");
+    enableDarkMode();
+    localStorage.setItem("darkMode", "on");
+  } else {
+    $(this).removeClass("active");
+    disableDarkMode();
+    localStorage.setItem("darkMode", "off");
+  }
+});

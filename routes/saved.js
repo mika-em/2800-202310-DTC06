69746f4d9c46ffffe09ab69ab2c3596792a17202
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-24 11:43:13 -0700

    Modify PDF output format and enable users to download to their own computer through web handling

--- routes/saved.js
+++ routes/saved.js
@@ -4,6 +4,7 @@ const User = require("../models/users");
 const Persona = require("../models/personaList");
 const pdfkit = require("pdfkit");
 const fs = require("fs");
+const path = require("path");
 
 router.get('/saved', (req, res) => {
     res.render("../views/saved/saved");
@@ -43,43 +44,79 @@ router.post('/persona/saved-persona/dialogue-filters', (req, res) => {
     console.log(persona);
     personaList.push(persona);
 
-    res.render("../dialogue/dialogueFilters");
+    res.render("./dialogue/dialogueFilters");
 });
 
-router.post('/saved/persona/save-as-pdf', (req, res) => {
+router.post('/saved/persona/save-as-pdf', async (req, res) => {
+    // Encode the personaList to handle special characters
     const personaList = req.body.personaList;
-    console.log(personaList);
+    const convertedPersonaList = JSON.parse(personaList.replaceAll('/n', '<br>'));
+    console.log(convertedPersonaList);
 
     // instantiate the library
     let doc = new pdfkit();
 
     // set font style
     doc.font('Courier');
-    
-    // pipe to a writable stream which would save the result into the same directory
-    doc.pipe(fs.createWriteStream('your_personas.pdf'));
 
-    doc.image('./images/invsona/invsona.png', { 
+    // Create an array to store the PDF chunks
+    const buffers = [];
+    // Create a unique filename for the user
+    const filename = `your-personas-${Date.now()}.pdf`;
+
+    // Set the response headers for downloading the PDF
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+
+    // Pipe the PDF stream directly to the response
+    doc.pipe(res);
+
+    doc.image('./images/invsona/invsona.png', {
         fit: [100, 100],
         align: 'center',
     });
 
-    doc.text('Your Personas by Invsona', { 
-        bold: true, 
+    doc.text('Your Personas by Invsona', {
         underline: true,
-        align: 'center' 
+        align: 'center'
+    });
+    doc.moveDown();
+    doc.moveDown();
+
+    for (let i = 0; i < convertedPersonaList.length; i++) {
+        doc.text('Persona ' + (i + 1), {
+            oblique: true,
+            align: 'center'
+        });
+        doc.text(convertedPersonaList[i]);
+        doc.moveDown();
+        doc.moveDown();
+    }
+
+    // Capture the output as buffers
+    doc.on('data', (chunk) => buffers.push(chunk));
+    doc.on('end', () => {
+        // Concatenate the buffers into a single buffer
+        const pdfBuffer = Buffer.concat(buffers);
+
+        // Send the PDF buffer as the response
+        res.end(pdfBuffer);
     });
 
-    // Encode the personaList to handle special characters
-    // const encodedPersonaList = personaList.replaceAll('/n', '<br>');
-
-    doc.text(personaList, { align: 'center' });
-
-    // write out file
+    // End the document
     doc.end();
 
-    // res.render("../views/saved/saved-persona");
-    res.send("PDF saved")
+    // render the page
+    const currentUser = await User.findOne({
+        username: req.session.user.username
+    });
+    const userID = currentUser._id;
+
+    const savedPersona = await Persona.find({
+        userId: userID
+    });
+
+    res.render("./saved/saved-persona", { savedPersona: savedPersona });
 });
 
 
